90aab4d6ec426a3cb9f5854117326abc05ce48e1
kaylahoo <790970276@qq.com>
2022-12-18 20:00:33 +0800

    rn

--- dataset.py
+++ dataset.py
@@ -89,7 +89,7 @@ class Dataset(torch.utils.data.Dataset):
         else:   # in test mode, there's a one-to-one relationship between mask and image; masks are loaded non random
             # mask = 255 - imread(self.mask_data[index])[:,:,0]    # ICME original (H,W,3) mask: 0 for hole
             mask = imageio.imread(self.mask_data[index])   # mask must be 255 for hole in this Inpainting Model
-            mask = self.resize(mask, imgh, imgw, centerCrop=True)
+            mask = self.resize(mask, imgh, imgw, centerCrop=False)
             if len(mask.shape) == 3:
                 mask = rgb2gray(mask)
         mask = (mask > 0).astype(np.uint8) * 255       # threshold due to interpolation
